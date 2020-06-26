89244a32499b0fdc926db287a3c6fc03f9f34842
unknown <peter@fasoo.com>
2020-06-26 09:37:21 +0900

    coupon entity column 설명

--- src/main/java/com/peterMoon/kakaoPay/entity/Coupon.java
+++ src/main/java/com/peterMoon/kakaoPay/entity/Coupon.java
@@ -22,33 +22,41 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor
 public class Coupon {
+	
+	// ID
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Long id;
 	
+	// 쿠폰 코드
 	@Column(name = "code", nullable = false, unique = true)
 	private String code;
 	
+	// 만료 일자
 	@Column(name = "expire_date", nullable = false, columnDefinition = "DATE")
 	private LocalDate expireDate;
 	
+	// 지급 여부
 	@Column(name = "issuance")
 	private Status issuance = Status.N;
 	
+	// 사용 여부
 	@Column(name = "use")
 	@Enumerated(EnumType.STRING)
 	private Status use = Status.N;
 	
+	// 쿠폰 지급 받은 사용자 메일
 	@Column(name = "mail")
 	private String mail;
 	
 	@Builder
-	public Coupon(Long id, String code, LocalDate expireDate, Status issuance, Status use) {
+	public Coupon(Long id, String code, LocalDate expireDate, Status issuance, Status use, String mail) {
 		this.id = id;
 		this.code = code;
 		this.expireDate = expireDate;
 		this.issuance = issuance;
 		this.use = use;
+		this.mail = mail;
 	}
 }
